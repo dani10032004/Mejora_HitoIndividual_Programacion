44c44b9ab66816a0e00c929dad2b4bac0bb38c2f
dani10032004 <125476299+dani10032004@users.noreply.github.com>
2023-04-10 12:22:57 +0200

    Add files via upload

---
+++ MejoraHitoProgramacion/formularioblog.php
@@ -0,0 +1,14 @@
+<?php
+
+$email=$_POST["email"];
+$titulo=$_POST["titulo"];
+$contenido=$_POST["contenido"];
+$fecha=$_POST["fecha"];
+$imagen=$_POST["imagen"];
+
+echo "los datos del formulario son los siguientes <br>";
+echo "$email <br>";
+echo "$titulo <br>";
+echo "$contenido <br>";
+echo "$fecha <br>";
+echo "$imagen <br>";
---
+++ MejoraHitoProgramacion/mostrarentradas.php
@@ -0,0 +1,37 @@
+<?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "clientes";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Selección de todas las entradas
+$sql = "SELECT * FROM `login`";
+$result = $conn->query($sql);
+
+// Mostrar las entradas
+if ($result->num_rows > 0) {
+    echo "<table border=1>";
+    echo "<tr><th>id</th><th>nombre</th><th>email</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["id"] . "</td>";
+        echo "<td>" . $row["nombre"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No se encontraron entradas";
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
